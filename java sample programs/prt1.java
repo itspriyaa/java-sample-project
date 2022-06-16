a3bce988ae639254bdc62aed1df83b9c0eb62ed1
itspriyaa <106379960+itspriyaa@users.noreply.github.com>
2022-06-16 16:49:01 +0530

    Add files via upload

---
+++ java sample programs/prt1.java
@@ -0,0 +1,19 @@
+public class prt1 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        int a=10;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+        System.out.println(10<<3);
+        /* 10*2*2*2*/
+        System.out.println(10>>3);
+        /* 10*2*2*2*/
+
+    }
+}
